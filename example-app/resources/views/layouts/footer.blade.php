5ee09ee0e8edb6cdf58357d543ae591848327fa3
Larsveneman <151181287+Larsveneman@users.noreply.github.com>
2024-10-08 13:15:53 +0200

    buttons en logo

--- example-app/resources/views/layouts/footer.blade.php
+++ example-app/resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
         <div class="container">
             <p>Follow us on:</p>
             <div class="social-icons">
-                <i class="fa fa-facebook-official"></i>
+                <i class="fa fa-facebook-official"> </i>
                 <i class="fa fa-instagram"></i>
                 <i class="fa fa-twitter"></i>
                 <i class="fa fa-linkedin"></i>
