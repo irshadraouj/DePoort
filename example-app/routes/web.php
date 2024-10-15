5a75a12e5bcb7bc266aa0d6f42a360975d11a03d
Baskuik <97105236@st.deltion.nl>
2024-10-15 14:34:22 +0200

    names toegevoegd

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,6 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
